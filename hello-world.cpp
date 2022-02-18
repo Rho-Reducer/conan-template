e64f0b027c39b3dbbcd9e83dc6823f6247842280
Fred Luke <fredricluke2@gmail.com>
2022-02-17 21:32:13 -0500

    hello-world program

---
+++ hello-world.cpp
@@ -0,0 +1,12 @@
+
+#include <hello-world.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+  std::cout << argv[0] << " Version " << hello_world_VERSION_MAJOR << "."
+    << hello_world_VERSION_MINOR << "." << hello_world_VERSION_PATCH << "\n";
+  std::cout << "Usage: " << argv[0] << "\n";
+  
+  return 0;
+}
